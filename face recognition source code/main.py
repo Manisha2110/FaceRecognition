1a318b3edbd35c80e89b108545618b9dfe4fef64
Manisha2110 <106418295+Manisha2110@users.noreply.github.com>
2022-05-29 17:19:01 +0530

    added files for face recognition

---
+++ face recognition source code/main.py
@@ -0,0 +1,17 @@
+
+import enroll,spreadsheet,emailing,recognition
+recognition.load_facial_encodings_and_names_from_memory()
+
+spreadsheet.mark_all_absent()
+
+recognition.run_recognition()
+
+
+
+#enroll.enroll_via_camera('Manisha')
+
+#spreadsheet.enroll_person_to_sheet('Manisha','Manisha.plmr@gmail.com')
+
+#emailing.email_pin('xxxx@gmail.com',1234)
+
+#
